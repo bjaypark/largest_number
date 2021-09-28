b40366d33fd65e0c836dd5234df4b769958fb4b7
bjaypark <fried.goguma@gmail.com>
2021-09-28 15:54:51 +0900

    Add files via upload

--- target_number.cpp
+++ target_number.cpp
@@ -11,26 +11,26 @@ int solution(vector<int> numbers, int target) {
     const int MAX = 1 << 20;
     int answer = 0;
     
-	newNum.reserve(MAX);
-	oldNum.reserve(MAX);
-	oldNum.emplace_back(0);
+    newNum.reserve(MAX);
+    oldNum.reserve(MAX);
+    oldNum.emplace_back(0);
 
-	for (const int num : numbers)
-	{
-		for (const int e : oldNum)
-		{
-			newNum.emplace_back(e + num);
-			newNum.emplace_back(e - num);
-		}
-		oldNum = newNum;
-		newNum.clear();
-	}
+    for (const int num : numbers)
+    {
+        for (const int e : oldNum)
+        {
+            newNum.emplace_back(e + num);
+            newNum.emplace_back(e - num);
+        }
+        oldNum = newNum;
+        newNum.clear();
+    }
 
-	for (const int e : oldNum)
-	{
-		if (e == target)
-			++answer;
-	}
+    for (const int e : oldNum)
+    {
+        if (e == target)
+            ++answer;
+    }
 
-	return answer;
+    return answer;
 }
